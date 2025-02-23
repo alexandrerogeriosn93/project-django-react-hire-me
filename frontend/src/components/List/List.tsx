f8a6e3c894ae5794a2092394d33525e71a4f1942
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2025-02-23 11:58:41 -0300

    feat: add List styles using styled component

--- frontend/src/components/List/List.tsx
+++ frontend/src/components/List/List.tsx
@@ -12,10 +12,18 @@ export const List = () => {
   return (
     <ListStyled>
       <ListItem>
-        <Photo src="https://github.com/xandaosilva.png"></Photo>
+        <Photo src="https://github.com/alexandrerogeriosn93.png"></Photo>
         <Informations>
           <Name>Alexandre Rogério</Name>
           <Value>R$100.00 por hora</Value>
+          <Description>Tecnólogo em Sistemas para Internet</Description>
+        </Informations>
+      </ListItem>
+      <ListItem>
+        <Photo src="https://github.com/django.png"></Photo>
+        <Informations>
+          <Name>Alexandre Rogério Silva Nunes</Name>
+          <Value>R$120.00 por hora</Value>
           <Description>Tecnólogo em Sistemas para Internet</Description>
         </Informations>
       </ListItem>
