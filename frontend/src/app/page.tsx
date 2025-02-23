a070148b60b54419d526169d9654dc6eb99093d5
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2025-02-23 12:42:02 -0300

    feat: create entity Professional

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,8 +3,26 @@
 import { ThemeProvider } from "@mui/material";
 import theme from "@/themes/theme";
 import { List } from "@/components/List/List";
+import { Professional } from "@/entities/professional";
 
 export default function Home({ children }: { children: React.ReactNode }) {
+  const professionals: Professional[] = [
+    {
+      id: 1,
+      name: "Alexandre",
+      description: "Tecnólogo em Sistemas para Internet",
+      value_per_hour: 100,
+      photo: "https://github.com/alexandrerogeriosn93.png",
+    },
+    {
+      id: 2,
+      name: "Django",
+      description: "Tecnologia para o desenvolvimento web",
+      value_per_hour: 300,
+      photo: "https://github.com/django.png",
+    },
+  ];
+
   return (
     <ThemeProvider theme={theme}>
       <List></List>
---
+++ frontend/src/entities/professional.ts
@@ -0,0 +1,7 @@
+export interface Professional {
+  id: number;
+  name: string;
+  description: string;
+  value_per_hour: number;
+  photo: string;
+}
