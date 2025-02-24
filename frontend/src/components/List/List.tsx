fb4b69e49131cfb37c3ec4eb0513ee573627bcdf
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2025-02-24 19:22:06 -0300

    feat: add condition to render professionals list, checking if list is empty

--- frontend/src/components/List/List.tsx
+++ frontend/src/components/List/List.tsx
@@ -1,6 +1,7 @@
 import { Professional } from "@/entities/professional";
 import {
   Description,
+  EmptyList,
   Informations,
   ListItem,
   ListStyled,
@@ -15,24 +16,30 @@ interface PropsList {
 
 export const List = (props: PropsList) => {
   return (
-    <ListStyled>
-      {props.professionals.map((professional) => (
-        <ListItem key={professional.id}>
-          <Photo src={professional.photo}></Photo>
-          <Informations>
-            <Name>{professional.name}</Name>
-            <Value>
-              {professional.value_per_hour.toLocaleString("pt-BR", {
-                minimumFractionDigits: 2,
-                style: "currency",
-                currency: "BRL",
-              })}{" "}
-              por hora
-            </Value>
-            <Description>{professional.description}</Description>
-          </Informations>
-        </ListItem>
-      ))}
-    </ListStyled>
+    <div>
+      {props.professionals.length > 0 ? (
+        <ListStyled>
+          {props.professionals.map((professional) => (
+            <ListItem key={professional.id}>
+              <Photo src={professional.photo}></Photo>
+              <Informations>
+                <Name>{professional.name}</Name>
+                <Value>
+                  {professional.value_per_hour.toLocaleString("pt-BR", {
+                    minimumFractionDigits: 2,
+                    style: "currency",
+                    currency: "BRL",
+                  })}{" "}
+                  por hora
+                </Value>
+                <Description>{professional.description}</Description>
+              </Informations>
+            </ListItem>
+          ))}
+        </ListStyled>
+      ) : (
+        <EmptyList>Não já profissionais cadastrados</EmptyList>
+      )}
+    </div>
   );
 };
