aae57cbb37347c2ba0b9274f9a4fdcd404dea838
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2025-03-01 08:14:21 -0300

    feat: create model Professional

--- backend/myjob/models.py
+++ backend/myjob/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Professional(models.Model):
+    name = models.CharField(max_length=80, null=False, blank=False)
+    photo = models.URLField(max_length=225, null=False, blank=False)
+    value_per_hour = models.DecimalField(
+        max_digits=9, decimal_places=2, null=False, blank=False
+    )
+    description = models.TextField(null=False, blank=False)
