7ec0216ec6220a01bae4b45cd25d852701c6e87f
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2025-03-01 11:31:35 -0300

    feat: create class Job

--- backend/myjob/models.py
+++ backend/myjob/models.py
@@ -8,3 +8,15 @@ class Professional(models.Model):
         max_digits=9, decimal_places=2, null=False, blank=False
     )
     description = models.TextField(null=False, blank=False)
+
+
+class Job(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+    email = models.EmailField(max_length=255, null=False, blank=False)
+    professional = models.ForeignKey(
+        to=Professional,
+        on_delete=models.CASCADE,
+        related_name="jobs",
+        null=False,
+        blank=False,
+    )
