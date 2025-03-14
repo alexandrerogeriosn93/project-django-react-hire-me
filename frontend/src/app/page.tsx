ccb53b7c5f2ad135276ddf72bf7dc142cbf8f588
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2025-03-14 02:19:54 -0300

    feat: create dialogue window to register a new job

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,15 @@
 "use client";
 
-import { ThemeProvider } from "@mui/material";
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  Grid2,
+  TextField,
+  ThemeProvider,
+} from "@mui/material";
 import theme from "@/themes/theme";
 import { List } from "@/components/List/List";
-import { Professional } from "@/entities/professional";
 import { useIndex } from "@/hooks/pages/useIndex";
 
 export default function Home({ children }: { children: React.ReactNode }) {
@@ -11,8 +17,28 @@ export default function Home({ children }: { children: React.ReactNode }) {
 
   return (
     <ThemeProvider theme={theme}>
-      <List professionals={listProfessionals}></List>
-      {children}
+      <div>
+        <List professionals={listProfessionals}></List>
+        <Dialog
+          open={true}
+          fullWidth
+          slotProps={{ paper: { sx: { padding: 6 } } }}
+        >
+          <Grid2 container spacing={2}>
+            <Grid2 size={12} component={"div"}>
+              <TextField label="Digite o nome" type="text" fullWidth />
+            </Grid2>
+            <Grid2 size={12} component={"div"}>
+              <TextField label="Digite o e-mail" type="email" fullWidth />
+            </Grid2>
+          </Grid2>
+          <DialogActions sx={{ mt: 5 }}>
+            <Button>Cancelar</Button>
+            <Button>Marcar trabalho</Button>
+          </DialogActions>
+        </Dialog>
+        {children}
+      </div>
     </ThemeProvider>
   );
 }
