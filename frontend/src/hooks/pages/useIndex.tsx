246e1f8ce275e2695cff0242064263c51d232b39
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2025-03-14 00:29:47 -0300

    feat: use Api Service to get data by api

--- frontend/src/hooks/pages/useIndex.tsx
+++ frontend/src/hooks/pages/useIndex.tsx
@@ -1,37 +1,17 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Professional } from "@/entities/professional";
+import { ApiService } from "@/services/ApiService";
 
 export function useIndex() {
-  const [listProfessionals, setListProfessionals] = useState<Professional[]>([
-    {
-      id: 1,
-      name: "Alexandre Rogério",
-      description: "Tecnólogo em Sistemas para Internet",
-      value_per_hour: 100,
-      photo: "https://github.com/alexandrerogeriosn93.png",
-    },
-    {
-      id: 2,
-      name: "Django",
-      description: "Tecnologia para o desenvolvimento backend",
-      value_per_hour: 300,
-      photo: "https://github.com/django.png",
-    },
-    {
-      id: 3,
-      name: "React",
-      description: "Tecnologia para o desenvolvimento frontend",
-      value_per_hour: 500,
-      photo: "https://github.com/react.png",
-    },
-    {
-      id: 4,
-      name: "Python",
-      description: "Linguagem de programação",
-      value_per_hour: 120,
-      photo: "https://github.com/python.png",
-    },
-  ]);
+  const [listProfessionals, setListProfessionals] = useState<Professional[]>(
+    []
+  );
+
+  useEffect(() => {
+    ApiService.get("/professionals").then((response) =>
+      setListProfessionals(response.data)
+    );
+  }, []);
 
   return { listProfessionals };
 }
